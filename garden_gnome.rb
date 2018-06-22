21eee43e9ed5f8778df0e727f6a9ac201d452aca
Kiara Winslow <kiarawinslow@gmail.com>
2018-06-22 21:40:06 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,30 @@
-# Code your instances here
+class GardenGnome
+ attr_reader :name #gets info
+ attr_writer :name #executes info
+ attr_reader :age
+ attr_writer :age
+ attr_reader :gluten_allergy
+ attr_writer :gluten_allergy
+ attr_reader :hat_color
+ attr_writer :hat_color
+ attr_reader :personality
+ 
+ def initialize(hat_color="red", personality="evil") #need to initialize bc i have a default variabler
+   @personality = "evil"
+   @hat_color = "red"
+ end
+   
+
+ def gnaw
+   return "Gnawing on a tree!!!"
+ end
+ 
+ def shout
+   return "GNARLY!!!"
+ end
+ 
+ def introduce_self
+   return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
+ end
+ 
+end
