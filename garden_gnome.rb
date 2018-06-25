42b157f55507754a465370298f5bedaf43167cae
emmakcompton <emmakcompton@gmail.com>
2018-06-25 16:15:38 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,48 @@
-# Code your instances here
+class GardenGnome
+
+  attr_accessor :name, :age, :gluten_allergy
+  attr_reader :personality
+
+  def initialize(hat_color="red")
+    @personality = "evil"
+    @hat_color = hat_color
+  end
+
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+
+  def hat_color
+    @hat_color
+  end
+
+  def shout
+   return "GNARLY!!!"
+  end
+
+  def introduce_self
+    return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+  end
+
+end
+  
+  
+
+
+
+
+
+
+
+
+  
+  
+  
+
+
+
+
+
+
+
+
